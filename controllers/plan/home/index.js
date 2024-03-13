e75e5658132b88ff0ebd948a48d4e1c4fe4a29ea
id00015933 <idd00015933@gmail.com>
2024-03-14 03:22:21 +0500

    template for add/update

--- controllers/plan/home/index.js
+++ controllers/plan/home/index.js
@@ -6,7 +6,16 @@ const homeController = {
         // This assumes that I am using a templating engine like EJS, Handlebars, Pug, etc.
         // The 'res.render' method is typically used to render dynamic content using a template engine
         res.render('home');
-    }  
+    },
+    // Function to render the add_update view for adding a new to-do item
+    add: async (req, res) =>{
+        res.render('home/add_update');
+    },
+    // Function to render the add_update view for updating an existing to-do item
+    update: async (req, res) =>{
+        res.render('home/add_update');
+    }
+
 };
 // Export the homeController object so that it can be imported and used in other parts of the application
 module.exports = homeController;
--- routes/plan/home/index.js
+++ routes/plan/home/index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 // Create a new router instance
 const router = express.Router();
-// Import the home controller module
 const homeController = require('../../../controllers/plan/home');
-// Define a route for the root //localhost:3000/
-// When a GET request is made to the root //localhost:3000/, the index function of the home controller will be called
+
 router.get('/', homeController.index);
+router.get('/add', homeController.add);
+router.get('/update', homeController.update);
 // Export the router to be used in other parts of the application
 module.exports = router;
